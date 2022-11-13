aaccce53c9dc96839c4072c01051077327674f14
Rick Caborn <git@lrcaborn.33mail.com>
2022-11-12 20:04:49 -0500

    Missed these in the last commit
    Signed-off-by: Rick Caborn <git@lrcaborn.33mail.com>

--- src/run.py
+++ src/run.py
@@ -2,7 +2,8 @@
 import pprint
 import argparse
 
-from PyRM import PyRM, Config
+from PyRM.PyRM import PyRM
+from PyRM.configs import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--config_file", "-f", help="configuration to use", type=str, dest="config_file")
@@ -13,14 +14,17 @@ config_file = results.config_file
 file_count = int(results.file_count)
 
 for i in range(file_count):
-  improv_config = Config.ImprovConfig()
-  #improv_config.track_configs = [Config.DrumEzxJazz()]
+  improv_config = ConfigImprov.ConfigImprov()
+  improv_config.track_configs = [DrumEzxJazz.DrumEzxJazz(improv_config.ticks_per_quarternote)]
   #improv_config.track_configs = [Config.DrumBlues()]
-  improv_config.track_configs = [Config.DrumHell()]
+  #improv_config.track_configs = [Config.DrumHell()]
   #improv_config.track_configs = [Config.DrumVintage1963()]
-  #improv_config.track_configs = [Config.PianoLongChords()]
+  #improv_config.track_configs = [Config.PianoLongNotes()]
+  #improv_config.track_configs = [Config.PadPiano()]
   #improv_config.track_configs = [Config.OrnamentPiano()]
+  #improv_config.track_configs = [Config.Drum808()]
   #improv_config.track_configs = [Config.CompPiano()]
+
   #improv_config.track_configs = [Config.Drum808()]
   #improv_config.track_configs = [Config.Vibes()]
   #improv_config.track_configs = [Config.DrumVintage1963()]
@@ -28,7 +32,7 @@ for i in range(file_count):
   #improv_config.track_configs = [Config.DrumEzxJazz(), Config.PadPiano()]
   #improv_config.track_configs = [Config.DrumEzxJazz(), Config.CompPiano(), Config.Vibes()]
   #improv_config.track_configs = [Config.PianoLongChords(), Config.Vibes()]
-  pyrm = PyRM.PyRM(improv_config)
+  pyrm = PyRM(improv_config)
   pyrm.build_tracks()
   pyrm.convert_tracks_to_midi_objects()
   midi_file = pyrm.export_to_midi_files(str(i))
